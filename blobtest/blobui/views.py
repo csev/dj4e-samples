330d7da8d0e13986f90caa332e12c6536108af71
Charles Severance <csev@umich.edu>
2019-01-22 14:36:03 -0500

    Progress on blobs

--- blobtest/blobui/views.py
+++ blobtest/blobui/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import logging
 
+import blobs.utils
+
 logger = logging.getLogger(__name__)
 
 # Create your views here.
@@ -27,18 +29,17 @@ from .forms import UploadFileForm
 # Imaginary function to handle an uploaded file.
 # from somewhere import handle_uploaded_file
 
+from random import *
+
 def upload_file(request):
     if request.method == 'POST':
         print('Title = '+request.POST['title'])
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
+            appkey = int(random() * 999999)
+            blobs.utils.handle_uploaded_file(request.FILES['file'], 'testing', appkey)
             return HttpResponseRedirect('/success')
     else:
         form = UploadFileForm()
     return render(request, 'upload.html', {'form': form})
 
-def handle_uploaded_file(f):
-    with open('/tmp/file.bin', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
