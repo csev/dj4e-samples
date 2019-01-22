ceb7a62d5ade5f5ddfe17cfbf0ed0dddf2407b44
Charles Severance <csev@umich.edu>
2019-01-22 12:56:02 -0500

    Make some progress

--- blobtest/blobui/urls.py
+++ blobtest/blobui/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('success', views.success, name='success'),
+    path('upload', views.upload_file, name='upload_file'),
 ]
 
--- blobtest/blobui/views.py
+++ blobtest/blobui/views.py
@@ -10,6 +10,35 @@ def index(request):
     <p>This sample code is available at
     <a href="https://github.com/csev/dj4e-samples">
     https://github.com/csev/dj4e-samples</a></p>
+    <p><a href="upload">Upload form</a></p>
     </body></html>"""
     return HttpResponse(response)
 
+def success(request):
+    response = """<html><body><p>Success</p>
+    <p><a href="..">Start over</a></p>
+    </body></html>"""
+    return HttpResponse(response)
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
+
+# Imaginary function to handle an uploaded file.
+# from somewhere import handle_uploaded_file
+
+def upload_file(request):
+    if request.method == 'POST':
+        print('Title = '+request.POST['title'])
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
+
+def handle_uploaded_file(f):
+    with open('/tmp/file.bin', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
