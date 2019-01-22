597e4234eb6e4a621d6374d24da423fb2423cc53
Charles Severance <csev@umich.edu>
2019-01-22 10:54:41 -0500

    Update the views in case they are live.

--- hello2/home/views.py
+++ hello2/home/views.py
@@ -9,6 +9,9 @@ def index(request):
     logging.error('Hello world DJ4E in the log...')
     print('Hello world DJ4E in a print statement...')
     response = """<html><body><p>Hello world DJ4E in HTML</p>
+    <p>This sample code is available at
+    <a href="https://github.com/csev/dj4e-samples">
+    https://github.com/csev/dj4e-samples</a></p>
     </body></html>"""
     return HttpResponse(response)
 
