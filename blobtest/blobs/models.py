3080f306c0df3572febd98537cee9b0ae0c74b9a
Charles Severance <csev@umich.edu>
2019-01-22 11:57:36 -0500

    First cut at the models.

--- blobtest/blobs/models.py
+++ blobtest/blobs/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blob(models.Model):
+    sha256 = models.CharField(max_length=64, help_text='The Checksum of the File.')
+    deleted = models.BooleanField()
+    content = models.BinaryField()
+    # https://stackoverflow.com/questions/3429878/automatic-creation-date-for-django-model-form-objects
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class File(models.Model):
+    sha256 = models.CharField(max_length=64, help_text='The Checksum of the File.')
+    blob = models.ForeignKey('Blob', on_delete=models.CASCADE)
+    location = models.CharField(max_length=1024, help_text='The location of this file if it is not a blob')
+    deleted = models.BooleanField()
+    contenttype = models.CharField(max_length=256, help_text='The MIMEType of the file when first uploaded')
+    name = models.CharField(max_length=256, help_text='The name of the file when first uploaded.')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    accessed_at = models.DateTimeField()
+    json = models.TextField()
+    application = models.CharField(max_length=256, help_text='The application that created this file')
+    applicationkey = models.IntegerField(help_text='A key within that application')
+
--- blobtest/blobtest/settings.py
+++ blobtest/blobtest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h^-(yaqm8j_geb7n^o@mj(3k&fk-91qjee7=k8y(r^4g)5d!qa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blobs.apps.BlobsConfig',
 ]
 
 MIDDLEWARE = [
