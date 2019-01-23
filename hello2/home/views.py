4ee82e1a2c21045b2664be3c15a65ea2f7087a70
Charles Severance <csev@umich.edu>
2019-01-23 16:47:14 -0500

    Add a 2

--- hello2/home/views.py
+++ hello2/home/views.py
@@ -6,9 +6,9 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 def index(request):
-    logging.error('Hello world DJ4E in the log...')
-    print('Hello world DJ4E in a print statement...')
-    response = """<html><body><p>Hello world DJ4E in HTML</p>
+    logging.error('Hello world (2) DJ4E in the log...')
+    print('Hello world DJ4E (2) in a print statement...')
+    response = """<html><body><p>Hello world DJ4E (2) in HTML</p>
     <p>This sample code is available at
     <a href="https://github.com/csev/dj4e-samples">
     https://github.com/csev/dj4e-samples</a></p>
