5a70bb270b72f1de30efe74cc5e8f330ce9cd809
Charles Severance <csev@umich.edu>
2019-01-31 21:05:12 -0500

    Cleanup

--- hello3/hello3/urls.py
+++ hello3/hello3/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
-from django.urls import include
 from home import views
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('admin/', admin.site.urls),
 ]
 
