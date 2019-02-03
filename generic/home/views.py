c62fe882d5755590bd113ca837c017569b78d45c
Charles Severance <csev@umich.edu>
2019-02-03 10:04:57 -0500

    Use the actual generic functions.

--- generic/home/views.py
+++ generic/home/views.py
@@ -1,3 +1,13 @@
+from home.models import Cat, Dog, Horse 
+
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views import generic
+
+class HorseListView(generic.ListView):
+    model = Horse
+
+class HorseDetailView(generic.DetailView):
+    model = Horse
