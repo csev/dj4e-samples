38e1acee8533faa6d8c28001acc5eb043c7693c4
Charles Severance <csev@umich.edu>
2019-02-03 10:21:56 -0500

    Add the cats...

--- generic/home/urls.py
+++ generic/home/urls.py
@@ -5,10 +5,10 @@ from django.views.generic import TemplateView
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
     path('', TemplateView.as_view(template_name='main.html')),
-    # path('cats', views.CatListView.as_view()),
-    # path('cat/<pk:pk>', views.CatDetailView.as_view()),
+    path('cats', views.CatListView.as_view()),
+    path('cat/<int:pk_from_url>', views.CatDetailView.as_view()),
     # path('dogs', views.DogListView.as_view()),
-    # path('dog/<pk:pk>', views.DogDetailView.as_view()),
+    # path('dog/<int:pk>', views.DogDetailView.as_view()),
     path('horses', views.HorseListView.as_view(), name='horses-all-view'),
     path('horse/<int:pk>', views.HorseDetailView.as_view()),
 ]
--- generic/home/views.py
+++ generic/home/views.py
@@ -1,9 +1,23 @@
 from home.models import Cat, Dog, Horse 
 
+from django.views import View
 from django.shortcuts import render
 
 # Create your views here.
 
+class CatListView(View):
+    def get(self, request) :
+        stuff = Cat.objects.all()
+        cntx = { 'cat_list': stuff }
+        return render(request, 'cat_list.html', cntx)
+    
+class CatDetailView(View):
+    def get(self, request, pk_from_url) :
+        obj = Cat.objects.get(pk=pk_from_url)
+        cntx = { 'cat': obj }
+        return render(request, 'cat_detail.html', cntx)
+
+# Lets rely on built-in generics
 from django.views import generic
 
 class HorseListView(generic.ListView):
