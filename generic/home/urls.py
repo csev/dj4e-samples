0580ada46a7527baded44c8f10a816fe92f43f97
Charles Severance <csev@umich.edu>
2019-02-03 09:25:11 -0500

    Initial commit

---
+++ generic/home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+# https://docs.djangoproject.com/en/2.1/topics/http/urls/
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='main.html')),
+    # path('remain/<slug:guess>', views.RestMainView.as_view()),
+]
+
