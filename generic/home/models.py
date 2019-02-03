49bdaebcc119b72870efde8e8628447c3a56e3ec
Charles Severance <csev@umich.edu>
2019-02-03 09:27:53 -0500

    Add some models and make migrations.

--- generic/home/models.py
+++ generic/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Cat(models.Model):
+    name = models.CharField(max_length=128)
+
+class Dog(models.Model):
+    name = models.CharField(max_length=128)
+
+class Horse(models.Model):
+    name = models.CharField(max_length=128)
+
