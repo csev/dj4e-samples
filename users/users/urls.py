6b4347b77359e388a2938a4b62f2cd05d5d6bab6
Charles Severance <csev@umich.edu>
2019-02-07 22:29:30 -0500

    More cleanup

--- users/users/urls.py
+++ users/users/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 from django.urls import include
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('admin/', admin.site.urls),
 ]
