e2411a52bfbd2b94744ded967e99f16ee535dc1e
Charles Severance <csev@umich.edu>
2019-02-12 15:21:57 -0500

    Tweak

--- all/stop.sh
+++ all/stop.sh
@@ -1,5 +1,5 @@
 
 echo "This kills most all the DJangos running on this server..."
 
-ps -a | grep ' ./manage.py runserver 80[0-9][0-9]' | awk '{print "kill " $1}' | bash
+ps -ewww | grep ' ./manage.py runserver 80[0-9][0-9]' | awk '{print "kill " $1}' | bash
 
