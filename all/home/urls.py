3906ce15bca9b685e6b113ecd2d28760bf4c5c8c
Charles Severance <csev@umich.edu>
2019-02-12 15:09:07 -0500

    Tweaking all

--- all/home/urls.py
+++ all/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main.html')),
+    path('', views.main),
 ]
 
--- all/home/views.py
+++ all/home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(request) :
+    host = request.META['HTTP_HOST']   # 127.0.0.1:8000
+    pos = host.find(':')
+    if pos > 0 :
+        host = host[0:pos]
+    ctx = { 'host': host}
+    return render(request, 'main.html', ctx)
+
