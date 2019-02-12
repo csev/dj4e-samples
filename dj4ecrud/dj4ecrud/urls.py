e83dc38cf7f7a625e3a4bef27f21a825440759e5
Charles Severance <csev@umich.edu>
2019-02-12 08:16:31 -0500

    Rename once again.

--- dj4ecrud/dj4ecrud/urls.py
+++ dj4ecrud/dj4ecrud/urls.py
@@ -1,4 +1,4 @@
-"""crudcode URL Configuration
+"""dj4ecrud URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- dj4ecrud/dj4ecrud/wsgi.py
+++ dj4ecrud/dj4ecrud/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crudcode project.
+WSGI config for dj4ecrud project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crudcode.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dj4ecrud.settings")
 
 application = get_wsgi_application()
