5bee50806f11a23c84d223b5363c9cd7c731fc8e
Charles Severance <csev@umich.edu>
2019-02-12 08:30:41 -0500

    Updates

--- dj4ecrud/home/urls.py
+++ dj4ecrud/home/urls.py
@@ -4,8 +4,6 @@ from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main.html')),
-    # path('cats', views.CatListView.as_view()),
-    # path('cat/<int:pk_from_url>', views.CatDetailView.as_view()),
+    path('', TemplateView.as_view(template_name='main.html'), name='main'),
 ]
 
