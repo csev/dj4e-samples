01f23c80aab506343a60262664b8d3a7e54ab6bd
Charles Severance <csev@umich.edu>
2019-02-12 10:17:48 -0500

    Add the cleanup bits

--- dj4ecrud/autos/views.py
+++ dj4ecrud/autos/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-from autos.models import Auto, Make
 
+from autos.models import Auto, Make
 
 # Create your views here.
 
 class MainView(View) :
     def get(self, request):
+        do_cleanup(request)  # ignore this :)
         mc = Make.objects.all().count();
         al = Auto.objects.all();
 
@@ -51,3 +52,21 @@ class MakeDelete(LoginRequiredMixin, DeleteView):
     model = Make
     fields = '__all__'
     success_url = reverse_lazy('autos')
+
+
+# Ignore this - clean out old entries when running on the web
+# Clean up if this is running anywhere other than localhost
+
+# https://stackoverflow.com/questions/18622007/runtimewarning-datetimefield-received-a-naive-datetime
+# https://stackoverflow.com/questions/10345147/django-query-datetime-for-objects-older-than-5-hours
+
+from datetime import datetime, timedelta
+from django.utils import timezone
+import datetime
+
+def do_cleanup(request) :
+    host = request.META['HTTP_HOST']
+    # if host.startswith('128.0.0.1') or host.startswith('localhost') : return
+    time_threshold = datetime.datetime.now(tz=timezone.utc) - timedelta(hours=1)
+    Auto.objects.filter(created_at=time_threshold).delete()
+    Make.objects.filter(created_at=time_threshold).delete()
