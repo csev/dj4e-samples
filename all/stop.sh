3eb28e6cb331ccbe9c32f20eeb76f86f9debbb11
Charles Severance <csev@umich.edu>
2019-02-12 16:41:22 -0500

    Update back to mac

--- all/stop.sh
+++ all/stop.sh
@@ -7,15 +7,14 @@ case "${unameOut}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${unameOut}"
 esac
-echo ${machine}
 
 djangos=`ps -efwww | grep ' ./manage.py runserver 0.0.0.0:80[0-9][0-9]' | wc -l`
 
 echo Killing $djangos Djangos on this server...
-# sleep 5
+sleep 5
 
 if [ "$machine" == "Linux" ] ; then
     ps -efwww | grep ' ./manage.py runserver 0.0.0.0:80[0-9][0-9]' | awk '{print "kill " $2}' | sh
 else
-    echo "Not yet"
+    ps -ewww | grep ' ./manage.py runserver 0.0.0.0:80[0-9][0-9]' | awk '{print "kill " $1}' | sh
 fi
