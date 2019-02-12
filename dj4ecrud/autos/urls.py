31a0c854125e3b66ee9e2cb9a98422746aedb910
Charles Severance <csev@umich.edu>
2019-02-12 08:37:30 -0500

    Move the template.  Sheesh.

--- dj4ecrud/autos/urls.py
+++ dj4ecrud/autos/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='autos/index.html')),
     # path('cats', views.CatListView.as_view()),
     # path('cat/<int:pk_from_url>', views.CatDetailView.as_view()),
 ]
