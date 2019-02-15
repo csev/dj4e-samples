f143d362031877d51fb77af5c3a64701d7a5fd68
Charles Severance <csev@umich.edu>
2019-02-14 20:38:04 -0500

    Cleanup on Aisle 6

--- dj4ecrud/autos/models.py
+++ dj4ecrud/autos/models.py
@@ -10,7 +10,6 @@ class Make(models.Model):
             help_text='Enter a make (e.g. Dodge)',
             validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
     )
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """String for representing the Model object."""
@@ -20,10 +19,10 @@ class Auto(models.Model) :
     nickname = models.CharField(
             max_length=200,
             validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
-   )
-    make = models.ForeignKey('Make', on_delete=models.CASCADE, null=False)
+    )
     mileage = models.PositiveIntegerField()
     comments = models.CharField(max_length=300)
+    make = models.ForeignKey('Make', on_delete=models.CASCADE, null=False)
 
     # Shows up in the admin list
     def __str__(self):
