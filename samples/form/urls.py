c3f82b4ea72f48b2c6b987921aef7d0cba7d4d68
Charles Severance <csev@umich.edu>
2019-02-16 13:35:44 -0500

    More cool code gone forever...

--- samples/form/urls.py
+++ samples/form/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('example', views.example),
     path('create', views.SimpleCreate.as_view()),
     path('update', views.SimpleUpdate.as_view()),
-    path('validate', views.SimpleValidate.as_view()),
-    path('postredirect', views.RedirectValidate.as_view()),
-    path('postredirect2', views.RedirectValidate2.as_view()),
+    path('validate', views.Validate.as_view()),
 ]
--- samples/form/views.py
+++ samples/form/views.py
@@ -36,30 +36,8 @@ class SimpleUpdate(DumpPostView):
         ctx = {'form' : form}
         return render(request, 'form.html', ctx)
 
-class SimpleValidate(DumpPostView):
+class Validate(DumpPostView):
     def get(self, request) :
-        old_data = {
-            'title': 'SakaiCar', 
-            'mileage' : 42, 
-            'purchase_date': '2018-08-14'
-        }
-        form = BasicForm(initial=old_data)
-        ctx = {'form' : form}
-        return render(request, 'form.html', ctx)
-
-    def post(self, request) :
-        form = BasicForm(request.POST)
-        form.is_valid() # We are going to print no matter what...
-        js = json.dumps(request.POST, sort_keys=True, indent=4)        
-        ctx = {'title': 'request.POST', 'dump': js, 'form': form}
-        return render(request, 'formdump.html', ctx)
-
-class RedirectValidate(DumpPostView):
-    def get(self, request) :
-        # Check if we have been redirected...
-        redirect_html = request.session.pop('form_error_html', False)
-        if redirect_html : return HttpResponse(redirect_html)
-
         old_data = {
             'title': 'SakaiCar', 
             'mileage' : 42, 
@@ -73,37 +51,7 @@ class RedirectValidate(DumpPostView):
         form = BasicForm(request.POST)
         if not form.is_valid() :
             ctx = {'form' : form}
-            html = render_to_string('form.html', ctx, request=request)
-            request.session['form_error_html'] = html
-            return redirect(request.path)
-
-        js = json.dumps(request.POST, sort_keys=True, indent=4)        
-        ctx = {'title': 'request.POST', 'dump': js}
-        return render(request, 'formdump.html', ctx)
-
-# Pass only the errors back in a list (old school)
-class RedirectValidate2(DumpPostView):
-    def get(self, request) :
-        old_data = {
-            'title': 'SakaiCar', 
-            'mileage' : 42, 
-            'purchase_date': '2018-08-14'
-        }
-        old_data = request.session.pop('form_post_data', old_data)
-        form_errors = request.session.pop('form_post_errors', False)
-
-        form = BasicForm(initial=old_data)
-
-        ctx = {'form' : form, 'errors': form_errors}
-        return render(request, 'formerrors.html', ctx)
-
-    def post(self, request) :
-        form = BasicForm(request.POST)
-        pprint(form)
-        if not form.is_valid() :
-            request.session['form_post_data'] = request.POST
-            request.session['form_post_errors'] = get_form_errors(form)
-            return redirect(request.path)
+            return render(request, 'form.html', ctx)
 
         js = json.dumps(request.POST, sort_keys=True, indent=4)        
         ctx = {'title': 'request.POST', 'dump': js}
@@ -111,5 +59,4 @@ class RedirectValidate2(DumpPostView):
 
 # References
 
-# https://stackoverflow.com/questions/14647723/django-forms-if-not-valid-show-form-with-error-message
 # https://stackoverflow.com/questions/383944/what-is-a-python-equivalent-of-phps-var-dump
