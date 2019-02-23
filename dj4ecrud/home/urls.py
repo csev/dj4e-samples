72b05f7f276aef12d87a4b8fa872487061547800
Charles Severance <csev@umich.edu>
2019-02-22 21:00:35 -0500

    Match hello

--- dj4ecrud/home/urls.py
+++ dj4ecrud/home/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main.html'), name='main'),
+    path('', TemplateView.as_view(template_name='main_home.html'), name='main'),
 ]
 
