2dbf3dc6e57ad43e251b4abcfd73e6dc1672e5c4
Charles Severance <csev@umich.edu>
2019-02-28 19:24:56 -0500

    Rename to reduce confusion.

--- onepage/onepage/urls.py
+++ onepage/onepage/urls.py
@@ -1,4 +1,4 @@
-"""dj4e URL Configuration
+"""onepage URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
