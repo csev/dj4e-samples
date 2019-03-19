1d7b79d45d71e9738f821f918f00c104dafb4757
Charles Severance <csev@umich.edu>
2019-03-18 23:09:21 -0400

    Cleanup

--- dj4elist/ads/urls.py
+++ dj4elist/ads/urls.py
@@ -8,17 +8,3 @@ urlpatterns = [
 
 ]
 
-import os
-from django.urls import re_path
-from django.views.static import serve
-from django.conf import settings
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^ads/static/(?P<path>.*)$', serve, {
-            'document_root': os.path.join(BASE_DIR, 'ads/static'),
-        }),
-    ]
-
