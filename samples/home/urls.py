77042498a23b726f162098f368f92ed401489f6d
Charles Severance <csev@umich.edu>
2019-03-19 05:36:24 -0400

    Move the favicon - fix the main page

--- samples/home/urls.py
+++ samples/home/urls.py
@@ -1,15 +1,6 @@
-import os
 from django.urls import path
 from . import views
-from django.views.static import serve
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 urlpatterns = [
-    path('', views.HomeView.as_view() ),
-    path('favicon.ico', serve, {
-            'path': 'favicon.ico',
-            'document_root': os.path.join(BASE_DIR, 'home/static'),
-        }
-    ),
+    path('', views.HomeView.as_view() )
 ]
--- samples/samples/urls.py
+++ samples/samples/urls.py
@@ -52,3 +52,15 @@ if 'social_django' in settings.INSTALLED_APPS :
         url(r'^oauth/', include('social_django.urls', namespace='social')),
     ]
 
+# Serve the favicon
+import os
+from django.views.static import serve
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
+]
+
