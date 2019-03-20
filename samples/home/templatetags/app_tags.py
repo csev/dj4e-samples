9557b71420f689ce66869f3fc66417ba24edf05b
Charles Severance <csev@umich.edu>
2019-03-20 08:42:57 -0400

    Evolve menu a bit, upgrade bootstrap

---
+++ samples/home/templatetags/app_tags.py
@@ -0,0 +1,18 @@
+from hashlib import md5
+from django import template
+
+# https://brobin.me/blog/2016/07/super-simple-django-gravatar/
+
+# A "gravatar" is a globally recognized avatar that is based on email address
+# People must register their email address and then upload a gravatar
+# If an email address has no gravatar, a generic image is put in its place
+
+register = template.Library()
+
+@register.filter(name='gravatar')
+def gravatar(user, size=35):
+    email = str(user.email.strip().lower()).encode('utf-8')
+    email_hash = md5(email).hexdigest()
+    url = "//www.gravatar.com/avatar/{0}?s={1}&d=identicon&r=PG"
+    return url.format(email_hash, size)
+
--- samples/samples/settings.py
+++ samples/samples/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'home.context_processors.settings',
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
             ],
