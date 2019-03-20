e83ee63cec1affc06cd9dcfc389de8a40c17ddb1
Charles Severance <csev@umich.edu>
2019-03-20 09:18:41 -0400

    As settings context

--- samples/samples/settings.py
+++ samples/samples/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Used for a default title
+APP_NAME = 'DJ4E Samples'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
