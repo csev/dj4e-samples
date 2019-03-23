30f3c5a9ee8a8e40b948b6e788d29e3047387dbf
Charles Severance <csev@umich.edu>
2019-03-23 15:39:00 -0400

    Tweak

--- samples/scripts/many_load.py
+++ samples/scripts/many_load.py
@@ -1,6 +1,8 @@
 
 import csv  # https://docs.python.org/3/library/csv.html
 
+# https://django-extensions.readthedocs.io/en/latest/runscript.html
+
 # python3 manage.py runscript many_load
 
 from many.models import Person, Course, Membership
