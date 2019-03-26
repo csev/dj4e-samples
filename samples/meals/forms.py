03aae5f880fdb268c5660fcdf7ad4485c7a55d0a
Charles Severance <csev@umich.edu>
2019-03-26 10:22:45 -0400

    Cleaner...

--- samples/meals/forms.py
+++ samples/meals/forms.py
@@ -1,8 +1,7 @@
-
-# https://docs.djangoproject.com/en/2.1/topics/http/file-uploads/
 from django import forms
 from meals.models import Meal
 
+# https://docs.djangoproject.com/en/2.1/topics/http/file-uploads/
 # https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
 # https://docs.djangoproject.com/en/2.1/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
 
@@ -10,6 +9,7 @@ from meals.models import Meal
 class CreateForm(forms.ModelForm):
     # Call this 'picture' so it gets copied from the form to the in-memory model
     # It will not be the "bytes", it will be the "InMemoryUploadedFile"
+    # because we need to pull out things like content_type
     picture = forms.FileField(required=False)
     max_upload_limit = 1 * 1024 * 1024
 
@@ -19,7 +19,8 @@ class CreateForm(forms.ModelForm):
 
     def clean(self) :
         cleaned_data = super().clean()
-        size = len(cleaned_data.get('picture'))
-        if size > self.max_upload_limit:
+        pic = cleaned_data.get('picture')
+        if pic is None : return
+        if len(pic) > self.max_upload_limit:
             self.add_error('picture', "File must be < "+str(self.max_upload_limit)+" bytes")
 
--- samples/meals/views.py
+++ samples/meals/views.py
@@ -1,8 +1,10 @@
 from django.views import View
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from owner.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
@@ -17,56 +19,55 @@ class MealDetailView(OwnerDetailView):
     model = Meal
     template_name = "meal_detail.html"
 
-class MealCreateView(LoginRequiredMixin, View):
+# This will handle create and update with an optional pk parameter on get and post
+class MealFormView(LoginRequiredMixin, View):
     template = 'meal_form.html'
     success_url = reverse_lazy('meals')
-    def get(self, request) :
-        form = CreateForm()
+    def get(self, request, pk=None) :
+        if not pk : 
+            form = CreateForm()
+        else: 
+            meal = get_object_or_404(Meal, id=pk, owner=self.request.user)
+            form = CreateForm(instance=meal)
         ctx = { 'form': form }
         return render(request, self.template, ctx)
 
-    def post(self, request) :
-        form = CreateForm(request.POST, request.FILES or None)
+    def post(self, request, pk=None) :
+        if not pk:
+            form = CreateForm(request.POST, request.FILES or None)
+        else:
+            meal = get_object_or_404(Meal, id=pk, owner=self.request.user)
+            form = CreateForm(request.POST, request.FILES or None, instance=meal)
+
         if not form.is_valid() :
             ctx = {'form' : form}
             return render(request, self.template, ctx)
 
+        # Adjust the model before saving
         meal = form.save(commit=False)
         meal.owner = self.request.user
-        f = meal.picture  # This is an django.core.files.uploadedfile.InMemoryUploadedFile
-        if f:  # Pull out the elements we need from InMemoryUploadedFile
+       
+        # The meal.picture can be one of three things
+        # If this is a create and a picture was not chosen, it is None
+        # If this is an update and with no prior picture and a new picture was not chosen, it is None
+        # If this is an update and with a prior picture and a new picture was not chosen, it is bytes
+        # If a picture was uploaded, it is a django.core.files.uploadedfile.InMemoryUploadedFile
+
+        f = meal.picture  
+        if isinstance(f, InMemoryUploadedFile):  # Pull out the elements we need
             bytearr = f.read();
-            print('We got a file ',f.name,'size='+str(len(bytearr)),'type='+f.content_type)
+            print('Received a file ',f.name,'size='+str(len(bytearr)),'type='+f.content_type)
             meal.picture = bytearr
             meal.content_type = f.content_type
+
         meal.save()
         return redirect(self.success_url)
-
-class XMealCreateView(OwnerCreateView):
-    model = Meal
-    fields = ['title', 'text']
-    template_name = "meal_form.html"
-
-    def form_valid(self, form):
-        print('views form_valid called')
-        object = form.save(commit=False)
-        fdata = self.request.FILES.get('file_data')
-        if fdata:
-            print('We got a file')
-        object.save()
-        return super(OwnerCreateView, self).form_valid(form)
-
-class MealUpdateView(OwnerUpdateView):
-    model = Meal
-    fields = ['title', 'text']
-    template_name = "meal_form.html"
 
 class MealDeleteView(OwnerDeleteView):
     model = Meal
     template_name = "meal_delete.html"
 
 
-from django.shortcuts import get_object_or_404
 def stream_file(request, pk) :
     meal = get_object_or_404(Meal, id=pk)
     response = HttpResponse()
