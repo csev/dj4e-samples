7286ae258cb7963c23973a00a60326e142462900
Charles Severance <csev@umich.edu>
2019-03-26 10:50:36 -0700

    Move naturalsize into a module

--- samples/meals/forms.py
+++ samples/meals/forms.py
@@ -1,24 +1,12 @@
 from django import forms
 from meals.models import Meal
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from meals.humanize import naturalsize
 
 # https://docs.djangoproject.com/en/2.1/topics/http/file-uploads/
 # https://stackoverflow.com/questions/2472422/django-file-upload-size-limit
 # https://stackoverflow.com/questions/32007311/how-to-change-data-in-django-modelform
 # https://docs.djangoproject.com/en/2.1/ref/forms/validation/#cleaning-and-validating-fields-that-depend-on-each-other
-
-# Really simple naturalsize
-def naturalsize(count) :
-    fcount = float(count)
-    k = 1024
-    m = k * k
-    g = m * k
-    if fcount < k : return str(count) + 'B'
-    if fcount >= k and fcount < m :
-        return str(int(fcount / (k/10.0) ) / 10.0) + 'KB'
-    if fcount >= m and fcount < g :
-        return str(int(fcount / (m/10.0) ) / 10.0) + 'MB'
-    return str(int(fcount / (g/10.0) ) / 10.0) + 'GB'
 
 # Create the form class.
 class CreateForm(forms.ModelForm):
---
+++ samples/meals/humanize.py
@@ -0,0 +1,13 @@
+
+# Really simple naturalsize that is missing from django humanize :(
+def naturalsize(count) :
+    fcount = float(count)
+    k = 1024
+    m = k * k
+    g = m * k
+    if fcount < k : return str(count) + 'B'
+    if fcount >= k and fcount < m :
+        return str(int(fcount / (k/10.0) ) / 10.0) + 'KB'
+    if fcount >= m and fcount < g :
+        return str(int(fcount / (m/10.0) ) / 10.0) + 'MB'
+    return str(int(fcount / (g/10.0) ) / 10.0) + 'GB'
