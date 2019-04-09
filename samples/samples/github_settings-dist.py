8cf7b6d29ab1c8c5384458333fe4bbaa0c716b13
Charles Severance <csev@umich.edu>
2019-04-09 15:48:42 -0400

    Update

--- samples/samples/github_settings-dist.py
+++ samples/samples/github_settings-dist.py
@@ -3,13 +3,22 @@
 
 # Go to https://github.com/settings/developers
 
-# Add a New OAuth2 App - GIve it a name
+# Add a New OAuth2 App 
 
-# If you are running on localhost, make the callback url be
-# http://127.0.0.1:8000/oauth/complete/github/
+# Using ngrok is hard because the url changes every time you start ngrok
 
-# If you are on the real internet (or using ngrok) make the callback url be
-# https://samples.dj4e.com/oauth/complete/github/
+# If you are running on localhost, here are some settings:
+
+# Application name: Django on Desktop
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
+# Using PythonAnywhere here are some settings:
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
 
 # Then copy the client_key and secret to this file
 
