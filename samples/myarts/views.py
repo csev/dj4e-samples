d80ed67e7da9bb8e2ae05d2ed82a740f4aa32115
Charles Severance <csev@umich.edu>
2019-04-19 13:14:53 -0400

    refactor template pattern

--- samples/myarts/views.py
+++ samples/myarts/views.py
@@ -8,23 +8,23 @@ from myarts.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerU
 
 class ArticleListView(OwnerListView):
     model = Article
-    template_name = "article_list.html"
+    template_name = "myarts/article_list.html"
 
 class ArticleDetailView(OwnerDetailView):
     model = Article
-    template_name = "article_detail.html"
+    template_name = "myarts/article_detail.html"
 
 class ArticleCreateView(OwnerCreateView):
     model = Article
     fields = ['title', 'text']
-    template_name = "article_form.html"
+    template_name = "myarts/article_form.html"
 
 class ArticleUpdateView(OwnerUpdateView):
     model = Article
     fields = ['title', 'text']
-    template_name = "article_form.html"
+    template_name = "myarts/article_form.html"
 
 class ArticleDeleteView(OwnerDeleteView):
     model = Article
-    template_name = "article_delete.html"
+    template_name = "myarts/article_delete.html"
 
