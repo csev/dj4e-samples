4260cef1749155cc1d05e382007983cc3e863555
Charles Severance <csev@umich.edu>
2019-04-20 11:17:31 -0400

    One more pattern refactor

--- samples/views/urls.py
+++ samples/views/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main_views.html')),
+    path('', TemplateView.as_view(template_name='views/main.html')),
     path('funky', views.funky),
     path('danger', views.danger),
     path('game', views.game),
