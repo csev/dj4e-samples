7f34950e8b3d028dc8ac884005ecf0fe6a418c3c
Charles Severance <csev@umich.edu>
2019-04-20 11:19:54 -0400

    More pattern

--- samples/session/urls.py
+++ samples/session/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main_session.html')),
+    path('', TemplateView.as_view(template_name='session/main.html')),
     path('cookie', views.cookie),
     path('sessfun', views.sessfun),
 ]
