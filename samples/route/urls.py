39c81389ccc078e9c3b4a7a82d803ec383ad0e58
Charles Severance <csev@umich.edu>
2019-04-23 09:28:30 -0400

    Updates

--- samples/route/urls.py
+++ samples/route/urls.py
@@ -7,7 +7,6 @@ from django.views.generic import TemplateView
 # Otherwise the name='' values will be global across all applications
 app_name = 'route'  
 
-# https://docs.djangoproject.com/en/2.1/topics/http/urls/
 urlpatterns = [
     path('', TemplateView.as_view(template_name='route/main.html')),
     path('first', views.FirstView.as_view(), name='first-view'),
@@ -16,4 +15,8 @@ urlpatterns = [
 
 # References
 
+# https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
 # https://docs.djangoproject.com/en/2.2/ref/urls/#include
+
+# https://docs.djangoproject.com/en/2.2/topics/http/urls/#url-namespaces-and-included-urlconfs
