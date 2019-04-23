8218902212dd27ce6405c07d9ffc9a0590030898
Charles Severance <csev@umich.edu>
2019-04-23 10:17:02 -0400

    Add app_name to menu

--- samples/menu/urls.py
+++ samples/menu/urls.py
@@ -3,9 +3,11 @@ from . import views
 from django.views.generic import TemplateView
 
 # https://docs.djangoproject.com/en/2.1/topics/http/urls/
+app_name='menu'
+
 urlpatterns = [
-    path('', views.TheView.as_view(), name='menu_main'),
-    path('page1', views.TheView.as_view(), name='menu_page1'),
-    path('page2', views.TheView.as_view(), name='menu_page2'),
-    path('page3', views.TheView.as_view(), name='menu_page3'),
+    path('', views.TheView.as_view(), name='main'),
+    path('page1', views.TheView.as_view(), name='page1'),
+    path('page2', views.TheView.as_view(), name='page2'),
+    path('page3', views.TheView.as_view(), name='page3'),
 ]
