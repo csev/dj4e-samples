7761eb61438ddc5ede0bc6edc35beb6d679c41b3
Charles Severance <csev@umich.edu>
2019-04-23 10:22:08 -0400

    Add app_name

--- samples/myarts/urls.py
+++ samples/myarts/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, reverse_lazy
 from . import views
 
+app_name='myarts'
 urlpatterns = [
-    path('', views.ArticleListView.as_view()),
-    path('articles', views.ArticleListView.as_view(), name='articles'),
+    path('', views.ArticleListView.as_view(), name='all'),
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article_detail'),
     path('article/create', 
-        views.ArticleCreateView.as_view(success_url=reverse_lazy('articles')), name='article_create'),
+        views.ArticleCreateView.as_view(success_url=reverse_lazy('myarts:all')), name='article_create'),
     path('article/<int:pk>/update', 
-        views.ArticleUpdateView.as_view(success_url=reverse_lazy('articles')), name='article_update'),
+        views.ArticleUpdateView.as_view(success_url=reverse_lazy('myarts:all')), name='article_update'),
     path('article/<int:pk>/delete', 
-        views.ArticleDeleteView.as_view(success_url=reverse_lazy('articles')), name='article_delete'),
+        views.ArticleDeleteView.as_view(success_url=reverse_lazy('myarts:all')), name='article_delete'),
 ]
 
