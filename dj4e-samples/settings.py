861058b4026d1a4b1cbcfa76cdcff85e6400132d
Charles Severance <csev@umich.edu>
2019-09-20 15:45:53 -0400

    Updates

--- dj4e-samples/settings.py
+++ dj4e-samples/settings.py
@@ -174,7 +174,7 @@ try:
     SOCIAL_AUTH_GITHUB_KEY = github_settings.SOCIAL_AUTH_GITHUB_KEY
     SOCIAL_AUTH_GITHUB_SECRET = github_settings.SOCIAL_AUTH_GITHUB_SECRET
 except:
-    print('If you want to use github for social log in, please see samples/github_settings-dist.py')
+    print('If you want to use github for social log in, please see dj4e-samples/github_settings-dist.py')
 
 # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html#authentication-backends
 # https://simpleisbetterthancomplex.com/tutorial/2016/10/24/how-to-add-social-login-to-django.html
