aacbcc461d5d6f5a544bbbabe91f22327c4eef10
Charles Severance <csev@umich.edu>
2019-09-24 06:57:49 -0400

    Moar route stuff

--- route/views.py
+++ route/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views import View
 
 class FirstView(View):  
     def get(self, request) :
-        return render(request, 'route/main.html', ctx)
+        return render(request, 'route/main.html')
 
 class SecondView(View):  
     def get(self, request) :
-        return render(request, 'route/main.html', ctx)
-
+        u = reverse_lazy('gview:cats')
+        u2 = reverse_lazy('gview:dogs')
+        u3 = reverse('gview:dog', args=['42'] )
+        ctx = {'x1' : u, 'x2': u2, 'x3': u3 }
+        return render(request, 'route/second.html', ctx)
 
 # References
 
