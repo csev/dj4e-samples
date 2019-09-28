361fad170fc9d92ae7a37d35275351ca4b9a30ad
Charles Severance <csev@umich.edu>
2019-09-28 10:17:02 -0400

    Tweaks

--- home/views.py
+++ home/views.py
@@ -4,6 +4,9 @@ from django.conf import settings
 
 # Create your views here.
 
+# This is a little complex because we need to detect when we are
+# running in various configurations
+
 class HomeView(View):
     def get(self, request) :
         print(request.get_host())
