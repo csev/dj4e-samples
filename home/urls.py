02fde9ea98775b55cba2d05451e4b6e98fac3e27
Charles Severance <csev@umich.edu>
2019-09-28 09:43:35 -0400

    Make it so we can support both social and simple login

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.HomeView.as_view() )
+    path('', views.HomeView.as_view() ),
 ]
+
+# The obtuse code below can be ignored - It cleverly switches 
+# between non-social login.html and social_login.html when we notice
+# that social login has been configured in settings.py (later in the course)
+
+from django.conf import settings
+try: 
+    if len(settings.SOCIAL_AUTH_GITHUB_KEY) > 0 :
+        social_login = 'registration/login_social.html'
+        urlpatterns += [
+            path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login)),
+        ]
+        print('Using',social_login,'as the login template')
+except:
+    print('Using registration/login.html as the login template')
+
--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 
-
 # Create your views here.
 
 class HomeView(View):
