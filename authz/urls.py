bceabd0ed5afcf462f4850eddebe5d9fb56fd635
Charles Severance <csev@umich.edu>
2019-09-28 18:43:40 -0400

    Refactor templates

--- authz/urls.py
+++ authz/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 app_name='authz'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='authz/main.html')),
+    path('', TemplateView.as_view(template_name='authz/base.html')),
     path('open', views.OpenView.as_view(), name='open'),
     path('protect', views.ProtectView.as_view(), name='protect'),
 ]
--- authz/views.py
+++ authz/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class OpenView(View) :
     def get(self, request):
-        return render(request, 'authz/main.html')
+        return render(request, 'authz/open.html')
 
 class ProtectView(LoginRequiredMixin, View) :
     def get(self, request):
-        return render(request, 'authz/main.html')
+        return render(request, 'authz/protect.html')
 
