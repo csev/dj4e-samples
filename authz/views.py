431ac57af200968642aa70887b754758785ab80c
Charles Severance <csev@umich.edu>
2019-09-28 22:53:05 -0400

    Add python dump

--- authz/views.py
+++ authz/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -14,4 +13,21 @@ class ApereoView(View) :
 class ProtectView(LoginRequiredMixin, View) :
     def get(self, request):
         return render(request, 'authz/protect.html')
+
+from django.http import HttpResponse
+
+class DoItInPython(View) :
+    def get(self, req):
+        resp = "<pre>\nUser Data in Python:\n\n"
+        if req.user.is_authenticated:
+            resp += "User: " + req.user.username + "\n"
+            resp += "Email: " + req.user.email + "\n"
+        else:
+            resp += "User is not logged in"
+        resp += "</pre>\n"
+        resp += """<p><a href="/authz">Go back</a></p>"""
+        return HttpResponse(resp)
+
+
+# https://docs.djangoproject.com/en/2.2/topics/auth/default/#authentication-in-web-requests
 
