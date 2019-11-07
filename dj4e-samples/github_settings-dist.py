b2388e604eb864f52f00b94f846ceed071ca0f35
Charles Severance <csev@umich.edu>
2019-11-07 14:54:07 -0500

    Updates

--- dj4e-samples/github_settings-dist.py
+++ dj4e-samples/github_settings-dist.py
@@ -9,13 +9,15 @@
 
 # If you are running on localhost, here are some settings:
 
-# Application name: Django on Desktop
+# Application name: ChuckList Local
 # Homepage Url: http://localhost:8000
 # Application Description: Whatever
 # Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
 
 
 # Using PythonAnywhere here are some settings:
+
+# Application name: ChuckList PythonAnywhere
 # Homepage Url: https://drchuck.pythonanywhere.com
 # Application Description: Whatever
 # Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
@@ -27,3 +29,4 @@
 
 SOCIAL_AUTH_GITHUB_KEY = '224642424242424230ee'
 SOCIAL_AUTH_GITHUB_SECRET = 'f1afce7ffa5424242424242424242412af40ec57'
+
