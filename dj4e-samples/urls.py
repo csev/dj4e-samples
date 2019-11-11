87e3e40c1866dc4f7cc602190878cf50e75a7ad0
Charles R. Severance <csev@umich.edu>
2019-11-11 21:53:08 +0000

    Move the switch code

--- dj4e-samples/urls.py
+++ dj4e-samples/urls.py
@@ -62,6 +62,27 @@ urlpatterns += [
     ),
 ]
 
+# The obtuse code below can be ignored - It dynamically switches
+# between non-social login.html and social_login.html when we notice
+# that social login has been configured in settings.py (later in the course)
+# Or just uncomment the path above when you enable social login
+
+from django.conf import settings
+try:
+    if len(settings.SOCIAL_AUTH_GITHUB_KEY) > 0 :
+        social_login = 'registration/login_social.html'
+        urlpatterns.insert(0,
+            path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+        )
+        print('Using',social_login,'as the login template for',settings.LOGIN_URL)
+except:
+    print('Using registration/login.html as the login template for',settings.LOGIN_URL)
+
+
+
+
+
+
 # References
 
 # https://docs.djangoproject.com/en/2.2/ref/urls/#include
