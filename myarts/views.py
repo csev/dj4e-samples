782c4de8d4819c0e8b66b0e234d0eecd11ac2de0
Charles Severance <csev@umich.edu>
2020-03-22 11:41:25 -0400

    MyArts cleanup

--- myarts/views.py
+++ myarts/views.py
@@ -1,30 +1,22 @@
 from myarts.models import Article
 
-from django.views import View
-from django.views import generic
-from django.shortcuts import render
-
 from myarts.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 class ArticleListView(OwnerListView):
     model = Article
-    template_name = "myarts/article_list.html"
+    # template_name = "myarts/article_list.html"
 
 class ArticleDetailView(OwnerDetailView):
     model = Article
-    template_name = "myarts/article_detail.html"
 
 class ArticleCreateView(OwnerCreateView):
     model = Article
     fields = ['title', 'text']
-    template_name = "myarts/article_form.html"
 
 class ArticleUpdateView(OwnerUpdateView):
     model = Article
     fields = ['title', 'text']
-    template_name = "myarts/article_form.html"
 
 class ArticleDeleteView(OwnerDeleteView):
     model = Article
-    template_name = "myarts/article_delete.html"
 
