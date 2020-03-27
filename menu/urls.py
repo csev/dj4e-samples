66e02440770c2b4eeb84f975921f0348146cf35a
Charles Severance <csev@umich.edu>
2020-03-26 23:03:41 -0400

    Updates

--- menu/urls.py
+++ menu/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from . import views
 from django.views.generic import TemplateView
 
-# https://docs.djangoproject.com/en/3.0/topics/http/urls/
 app_name='menu'
-
 urlpatterns = [
-    path('', views.TheView.as_view(), name='main'),
-    path('page1', views.TheView.as_view(), name='page1'),
-    path('page2', views.TheView.as_view(), name='page2'),
-    path('page3', views.TheView.as_view(), name='page3'),
+    path('', TemplateView.as_view(template_name='menu/main_menu.html'), name='main'),
+    path('page1', TemplateView.as_view(template_name='menu/main_menu.html'), name='page1'),
+    path('page2', TemplateView.as_view(template_name='menu/main_menu.html'), name='page2'),
+    path('page3', TemplateView.as_view(template_name='menu/main_menu.html'), name='page3'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,3 @@
-from django.shortcuts import render
 from django.views import View
 
 # Create your views here.
-
-class TheView(View) :
-    def get(self, request) :
-        x = { 'request' : request }
-        return render(request, 'menu/main_menu.html', x)
