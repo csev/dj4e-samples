28de652a84095f5775747b984f74f0e7622c092e
Charles Severance <csev@umich.edu>
2020-03-26 23:36:15 -0400

    Updates

--- crispy/forms.py
+++ crispy/forms.py
@@ -1,4 +1,3 @@
-
 from django import forms
 from django.core.exceptions import ValidationError
 from django.core import validators
@@ -10,11 +9,10 @@ class BasicForm(forms.Form):
     mileage = forms.IntegerField()
     purchase_date = forms.DateField()
 
-
-
 # References 
 
 # https://docs.djangoproject.com/en/3.0/ref/forms/api/
 # https://docs.djangoproject.com/en/3.0/ref/forms/fields/#datefield
 # https://docs.djangoproject.com/en/3.0/ref/forms/validation/#using-validation-in-practice
 # https://docs.djangoproject.com/en/3.0/ref/validators/
+
