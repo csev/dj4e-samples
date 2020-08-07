f9b7491bf87ff24d454487b933355fdeaefb2873
Sameh Farouk Abd Elmoaty Abou El Saad <42457449+sameh-farouk@users.noreply.github.com>
2020-08-08 00:37:30 +0200

    Update owner.py
    In Python 3, the super(Square, self) call is equivalent to the parameterless super() call. 
    The parameterless call to super() is recommended and sufficient for most use cases.

--- myarts/owner.py
+++ myarts/owner.py
@@ -25,7 +25,7 @@ class OwnerCreateView(LoginRequiredMixin, CreateView):
         object = form.save(commit=False)
         object.owner = self.request.user
         object.save()
-        return super(OwnerCreateView, self).form_valid(form)
+        return super().form_valid(form)
 
 class OwnerUpdateView(LoginRequiredMixin, UpdateView):
     """
@@ -36,7 +36,7 @@ class OwnerUpdateView(LoginRequiredMixin, UpdateView):
     def get_queryset(self):
         print('update get_queryset called')
         """ Limit a User to only modifying their own data. """
-        qs = super(OwnerUpdateView, self).get_queryset()
+        qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
 
 class OwnerDeleteView(LoginRequiredMixin, DeleteView):
@@ -47,7 +47,7 @@ class OwnerDeleteView(LoginRequiredMixin, DeleteView):
 
     def get_queryset(self):
         print('delete get_queryset called')
-        qs = super(OwnerDeleteView, self).get_queryset()
+        qs = super().get_queryset()
         return qs.filter(owner=self.request.user)
 
 # References
