4ee897a587d896204fec4ada2344e0851704a5b7
Eugene <eugene.schekutiev@gmail.com>
2021-01-09 16:09:58 -0800

    Remove import User
    Remove "from django.contrib.auth.models import User" line as we use AUTH_USER_MODEL to reference the User Model.

--- favsql/models.py
+++ favsql/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
 from django.conf import settings
 
 class Thing(models.Model) :
