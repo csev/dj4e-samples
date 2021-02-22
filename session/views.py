c859bbb173673fdf7c08b97410ff060bf719f25d
Charles Severance <csev@umich.edu>
2021-02-22 17:16:47 -0500

    Updates

--- session/views.py
+++ session/views.py
@@ -22,5 +22,6 @@ def sessfun(request) :
     num_visits = request.session.get('num_visits', 0) + 1
     request.session['num_visits'] = num_visits 
     if num_visits > 4 : del(request.session['num_visits'])
-    return HttpResponse('view count='+str(num_visits))
+    resp = HttpResponse('view count='+str(num_visits))
+    return resp
 
