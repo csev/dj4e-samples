506c7e8dfd9725bd512989f949f426be6864fd43
Charles R. Severance <csev@umich.edu>
2021-04-14 20:28:58 +0000

    Updates.

--- dj4e-samples/github_settings-dist.py
+++ dj4e-samples/github_settings-dist.py
@@ -5,16 +5,6 @@
 
 # Add a New OAuth2 App 
 
-# Using ngrok is hard because the url changes every time you start ngrok
-
-# If you are running on localhost, here are some settings:
-
-# Application name: ChuckList Local
-# Homepage Url: http://localhost:8000
-# Application Description: Whatever
-# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
-
-
 # Using PythonAnywhere here are some settings:
 
 # Application name: ChuckList PythonAnywhere
@@ -30,5 +20,21 @@
 SOCIAL_AUTH_GITHUB_KEY = '224642424242424230ee'
 SOCIAL_AUTH_GITHUB_SECRET = 'f1afce7ffa5424242424242424242412af40ec57'
 
+# Ask for the user's email (don't edit this line)
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+
+# Note you may not get email for github users that don't make their
+# email public - that is OK
+
 # For detail: https://readthedocs.org/projects/python-social-auth/downloads/pdf/latest/
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: ChuckList Local
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
 
