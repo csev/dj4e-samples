027b02c4512312f1ae5fa86e5535f903b248b8ef
Charles Severance <csev@umich.edu>
2022-04-11 17:42:47 -0400

    Remove unneeded import

--- pics/views.py
+++ pics/views.py
@@ -4,8 +4,6 @@ from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.core.files.uploadedfile import InMemoryUploadedFile
-
 from myarts.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 from pics.models import Pic
