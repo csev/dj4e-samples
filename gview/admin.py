e07aebdadcb8e3e852264f047135f18475406ca2
Charles R. Severance <csev@umich.edu>
2025-02-15 04:13:26 +0000

    Add to admin.py

--- gview/admin.py
+++ gview/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Cat, Horse, Dog, Car
+
 # Register your models here.
+
+admin.site.register(Cat)
+admin.site.register(Horse)
+admin.site.register(Dog)
+admin.site.register(Car)
+
+
+
