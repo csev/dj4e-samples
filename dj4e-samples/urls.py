bfcfa208ac6d59241598e20749b2d696625b4100
Charles Severance <csev@umich.edu>
2025-09-14 08:02:08 -0400

    Updates

--- dj4e-samples/urls.py
+++ dj4e-samples/urls.py
@@ -1,8 +1,5 @@
 """samples URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
