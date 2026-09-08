7d713928101d5231d8419e61337a232e26b44e9c
Sejal Ahlawat <148641950+ahlawatsejal@users.noreply.github.com>
2026-09-08 20:08:44 +0530

    Refactor session handling and introduce myview function
    Refactor cookie handling and session view count logic. Introduce myview function to track visits and set a cookie.

--- session/views.py
+++ session/views.py
@@ -1,27 +1,17 @@
 from django.http import HttpResponse
 
-# Create your views here.
-
-# https://docs.djangoproject.com/en/4.2/ref/request-response/#django.http.HttpRequest.COOKIES
-# HttpResponse.set_cookie(key, value='', max_age=None, expires=None, path='/', 
-#     domain=None, secure=None, httponly=False, samesite=None)
-def cookie(request):
-    print(request.COOKIES)
-    oldval = request.COOKIES.get('zap', None)
-    resp = HttpResponse('In a view - the zap cookie value is '+str(oldval))
-    if oldval : 
-        resp.set_cookie('zap', int(oldval)+1) # No expired date = until browser close
-    else : 
-        resp.set_cookie('zap', 42) # No expired date = until browser close
-    resp.set_cookie('sakaicar', 42, max_age=1000) # seconds until expire
-    return resp
-
-# https://www.youtube.com/watch?v=Ye8mB6VsUHw
-
-def sessfun(request) :
+def myview(request):
+    # Track and increment view count in session
     num_visits = request.session.get('num_visits', 0) + 1
-    request.session['num_visits'] = num_visits 
-    if num_visits > 4 : del(request.session['num_visits'])
-    resp = HttpResponse('view count='+str(num_visits))
+    request.session['num_visits'] = num_visits
+    
+    if num_visits > 4:
+        del request.session['num_visits']
+        
+    # Build response including the view count and required string
+    resp = HttpResponse(f'view count={num_visits} ff7b1297')
+    
+    # Set the required cookie with the matching code string
+    resp.set_cookie('dj4e_cookie', 'ff7b1297', max_age=1000)
+    
     return resp
-
